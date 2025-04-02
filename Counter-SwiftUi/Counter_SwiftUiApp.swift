27535dbc67ddf6af772e066cbd1118f4d82a082d
QL-ARYAN-SINGHAL <aryan.singhal@quokkalabs.com>
2025-04-02 10:48:48 +0530

    Initial Commit

---
+++ Counter-SwiftUi/Counter_SwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Counter_SwiftUiApp.swift
+//  Counter-SwiftUi
+//
+//  Created by ARYAN SINGHAL on 02/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Counter_SwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Counter-SwiftUiTests/Counter_SwiftUiTests.swift
@@ -0,0 +1,17 @@
+//
+//  Counter_SwiftUiTests.swift
+//  Counter-SwiftUiTests
+//
+//  Created by ARYAN SINGHAL on 02/04/25.
+//
+
+import Testing
+@testable import Counter_SwiftUi
+
+struct Counter_SwiftUiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

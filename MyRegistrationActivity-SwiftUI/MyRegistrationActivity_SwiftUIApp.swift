b096eb7ce07ab2fe8c57e856364ddc657f9a3b8c
QL-ARYAN-SINGHAL <aryan.singhal@quokkalabs.com>
2025-04-02 11:50:24 +0530

    Initial Commit

---
+++ MyRegistrationActivity-SwiftUI/MyRegistrationActivity_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRegistrationActivity_SwiftUIApp.swift
+//  MyRegistrationActivity-SwiftUI
+//
+//  Created by ARYAN SINGHAL on 02/04/25.
+//
+
+import SwiftUI
+
+@main
+struct MyRegistrationActivity_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyRegistrationActivity-SwiftUITests/MyRegistrationActivity_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  MyRegistrationActivity_SwiftUITests.swift
+//  MyRegistrationActivity-SwiftUITests
+//
+//  Created by ARYAN SINGHAL on 02/04/25.
+//
+
+import Testing
+@testable import MyRegistrationActivity_SwiftUI
+
+struct MyRegistrationActivity_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
